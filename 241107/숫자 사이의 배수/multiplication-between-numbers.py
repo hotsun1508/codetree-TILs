b86d8ce4221b59611875a18a75d1a20c4d97fb8f
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-07 23:56:18 +0900

    Title: 숫자 사이의 배수, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241107/숫자 사이의 배수/multiplication-between-numbers.py
+++ 241107/숫자 사이의 배수/multiplication-between-numbers.py
@@ -9,9 +9,25 @@ avg_val = 0
 
 for i in range(a, b+1):
     if i % 5 == 0 or i % 7 == 0:
+        # print("-- i: ", i)
         sum_val += i
         count += 1
-    if count != 0:
-        avg_val = sum_val / count
+        # print("sum_val: ", sum_val, "count :", count)
+    # if count != 0:
+avg_val = sum_val / count
+# print("avg_val: ", avg_val)
 
 print (sum_val, round(avg_val, 1))
+
+
+
+# a, b = map(int, input().split(" "))
+
+# num = 0
+# count = 0
+# for i in range(a, b+1, 1):
+#     if i % 5 == 0 or i % 7 == 0:
+#         num += i
+#         count += 1
+
+# print(num, round(num / count, 1))

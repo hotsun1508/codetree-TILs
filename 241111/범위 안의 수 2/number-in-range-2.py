c5b56e260d6bdf01b9b867addd6b05c83a858822
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-11 23:02:57 +0900

    Title: 범위 안의 수 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241111/범위 안의 수 2/number-in-range-2.py
@@ -0,0 +1,15 @@
+# 문제 이해
+    # 0 <= 정수 <= 200 이어야 함
+    # 위 조건을 만족하는 정수들의 합과 평균을 구해야 함!
+
+cnt = 0
+sum_val = 0
+
+for i in range(10):
+    i = int(input())
+
+    if 0 <= i <= 200:
+        sum_val += i
+        cnt += 1
+    
+print(sum_val, round(sum_val/cnt, 1))

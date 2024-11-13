36e603a7d1ab2c89b7cd8362c23d6db4172ed3a7
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-13 22:38:22 +0900

    Title: a의 b승, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241113/a의 b승/a-to-the-power-of-b.py
+++ 241113/a의 b승/a-to-the-power-of-b.py
@@ -1,5 +1,17 @@
-# 시간 복잡도: O(1)
+# 시간 복잡도: O(N) -> a를 b번 곱함
+
+# 방법 1
+# a, b = map(int, input().split())
+
+# print (a**b)
+
+
+# 방법 2
 
 a, b = map(int, input().split())
+prod = 1
 
-print (a**b)
+for _ in range(b):
+    prod *= a
+
+print(prod)

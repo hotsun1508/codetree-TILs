3e42ad31f1a19b7ad78993a7147b39de2e396e04
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-13 22:05:14 +0900

    Title: 입력받은 수의 합과 평균, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241113/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
+++ 241113/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
@@ -2,14 +2,14 @@
 
 
 n = int(input())
-cnt = 0
+# cnt = 0
 sumval = 0
 
 for i in range(n):
     i = int(input())
 
     sumval += i
-    cnt += 1
+    # cnt += 1
 
 # print(sumval, round(sumval/cnt, 1))
-print(f"{sumval} {sumval/cnt:.1f}")
+print(f"{sumval} {sumval/n:.1f}")

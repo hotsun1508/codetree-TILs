7b8303b3e9d75a752b26cb979ede10b6424a593e
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-23 01:09:17 +0900

    Title: 출석 부르기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241123/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(1)
+
+
+while True: 
+    n = int(input())
+
+    if n == 1: 
+        print("John")
+    elif n == 2:
+        print("Tom")
+    elif n == 3:
+        print("Paul")
+    elif n == 4:
+        print("Sam")
+    else: 
+        print("Vacancy")
+        break

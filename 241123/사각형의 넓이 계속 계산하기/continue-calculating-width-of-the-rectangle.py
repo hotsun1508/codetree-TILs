9bcc32777f8f2e9ed6f22c7217e944251eafd0d4
Sun Ah Min <sunaslight1508@gmail.com>
2024-11-23 01:28:18 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241123/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
+++ 241123/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
@@ -1,4 +1,4 @@
-# Time Complexity 
+# Time Complexity: O(1)
 
 
 while True: 
@@ -8,3 +8,19 @@ while True:
 
     if char == 'C':
         break 
+
+
+# Method 2. 
+# while(True):
+#     # Declare variables and receive input
+#     inp = input()
+#     arr = inp.split()
+#     w = int(arr[0])
+#     h = int(arr[1])
+#     a = arr[2]
+        
+#     # Print the area of the rectangle. If the character C is entered, terminate.
+#     print(w * h)
+        
+#     if a == 'C':
+#         break

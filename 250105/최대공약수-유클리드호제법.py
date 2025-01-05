8a3c47a8b8f72c4beebb2499a217b5871d9fbe53
Sun Ah Min <sunaslight1508@gmail.com>
2025-01-05 23:19:10 +0900

    Update 최대공약수-유클리드호제법.py
    파이썬 구현 방법 추가

--- 250105/최대공약수-유클리드호제법.py
+++ 250105/최대공약수-유클리드호제법.py
@@ -34,7 +34,49 @@ Algorithm
 이 문제의 코드는, 사실 두 수의 최대공약수를 구하는 코드이기도 합니다. 이는 '유클리드 호제법' 이라는 이름으로 잘 알려져 있습니다. 
 자세한 내용은 [여기] (https://ko.wikipedia.org/wiki/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95) 를 참고하세요.
 
-줄마다 변수값이 어떻게 변하는지를 살펴보면 아래와 같습니다.
 
+
+
+
+파이썬 구현 방법 
+
+1. 
+def gcd(m,n):
+	if m < n:
+		m, n = n, m
+	if n == 0:
+		return m
+    if m % n == 0:
+		return n
+	else:
+		return gcd(n, m%n)
+
+2. 
+def gcd(m,n):
+    while n != 0:
+       t = m%n
+       (m,n) = (n,t)
+    return abs(m)
+
+3. 
+def gcd(m,n):
+    while n! = 0:
+	    if m < n:
+		    m, n = n, m
+	    if n == 0:
+		    return m
+	    if m % n == 0:
+		    return n
+
+4.
+def gcd(m,n):
+    if n == 0:
+        return m
+    mod = m % n
+    if mod != 0:
+        m, n = n, mod
+        return gcd(m, n)
+    else:
+        return n
 
 

788991fff3e2d73c826268f93185ea25abfe62a5
Sun Ah Min <sunaslight1508@gmail.com>
2025-06-23 13:31:22 +0900

    Title: ab 사이에 있는 c, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250623/ab 사이에 있는 c/c-between-a-and-b.py
+++ 250623/ab 사이에 있는 c/c-between-a-and-b.py
@@ -2,9 +2,11 @@ a, b, c = map(int, input().split())
 satisfied = False
 
 for i in range(a, b+1):
+    # a,b 사이에 c의 배수가 있는지 확인 
     if i%c == 0:
         satisfied = True
-    
+
+# 출력 
 if satisfied == True:     
     print("YES")
 else:

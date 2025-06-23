7f9092c732d7246ef8f6dfbd04e051fc76b5c9e8
Sun Ah Min <sunaslight1508@gmail.com>
2025-06-23 13:44:15 +0900

    Title: 공약수의 유무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250623/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+satisfied = False
+
+for i in range(a, b+1):
+    # 공약수 존재하는지 확인 
+    # 8, 10: 
+        # 8: 1, 2, 4, 8
+        # 10: 1, 2, 5, 10 
+        # 공약수: 2 
+    if a % i == 0 and b % i == 0:  
+        satisfied = True
+    
+
+if satisfied == True:
+    print(1)
+

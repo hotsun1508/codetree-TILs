4603d479bce0b74ef6476934d042944d4b919e43
Sun Ah Min <sunaslight1508@gmail.com>
2025-06-23 13:39:04 +0900

    Title: 합성수 판별기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250623/합성수 판별기/factorization-discriminator.py
+++ 250623/합성수 판별기/factorization-discriminator.py
@@ -1,7 +1,8 @@
 n = int(input())
 satisfied = False
 
-for i in range (2, n-1):
+# n이라고 넣으면, n-1까지임 
+for i in range (2, n):
     # 주어진 정수가 n-1이하의 어떤 정수로 나누어 떨어지는지 확인 
     if n % i == 0: 
         satisfied = True

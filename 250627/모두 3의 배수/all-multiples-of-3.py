6a283f563fd84c6c5cf76f9a513c265a91c7d746
Sun Ah Min <sunaslight1508@gmail.com>
2025-06-27 10:52:24 +0900

    Title: 모두 3의 배수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250627/모두 3의 배수/all-multiples-of-3.py
+++ 250627/모두 3의 배수/all-multiples-of-3.py
@@ -1,15 +1,18 @@
-n = list(map(int, input().split("\n"))) # 방법 1: map 사용
-# input_str = int(input())
-# n = [int(input()) for _ in range(input_str)] # 방법 2: List comprehension
+# n = list(map(int, input().split("\n"))) # 방법 1: 이렇게는 안됨 
+# n = [int(input()) for _ in range(5)] # 방법 2: List comprehension
 satisfied = False
 
-for i in n:
+# for i in n:  # 방법 2로 할 경우 
+for i in [int(input()) for _ in range(5)]: # 방법 3: 바로 for loop에 list comprehension 넣기 
     if i%3 == 0:
+        # print(i, end='\n')
         satisfied = True
     else: 
         satisfied = False
+        # print(i, end='\n')
         break     # 1개라도 3의 배수가 아니면 break 해야함 
-    
+# print(satisfied)
+
 if satisfied == True:
     print(1)
 else:

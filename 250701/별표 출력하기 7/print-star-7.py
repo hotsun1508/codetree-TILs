a1d7b07c8e98f0944033c4053ffc4d72fe80d274
Sun Ah Min <sunaslight1508@gmail.com>
2025-07-01 20:40:39 +0900

    Title: 별표 출력하기 7, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250701/별표 출력하기 7/print-star-7.py
+++ 250701/별표 출력하기 7/print-star-7.py
@@ -1,5 +1,12 @@
 n = int(input())
 
-for i in range(1, n+1):
-    # print("n :", n)
-    print("* " * i)
+# 방법 1: 반복문 하나로 해결 
+# for i in range(1, n+1):
+#     # print("n :", n)
+#     print("* " * i)
+
+# 방법 2: 반복문 2개 사용 
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()

65aecc99971bbdcd01b08cec704a379e0b317fec
Sun Ah Min <sunaslight1508@gmail.com>
2025-07-08 13:40:36 +0900

    Title: 별표 출력하기, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250708/별표 출력하기/print-star.py
+++ 250708/별표 출력하기/print-star.py
@@ -1,35 +1,35 @@
 n = int(input())
 
 # 방법 1 
-# for i in range(1, n+1):
-#     # print("i :", i)
-#     for j in range(i):
-#         # print("j :", j)
-#         print("*", end=" ")
-#     print()
-
-# for i in range(1, n): 
-#     # print("i :", i)
-#     for j in range(n - i):
-#         # print("j :", j)
-#         print("*", end=" ")
-#     print()
-
-
-# 방법 2 
-cnt = 1
-# print("cnt :", cnt)
-
-for i in range(2*n-1): 
+for i in range(1, n+1):
     # print("i :", i)
-    for j in range(cnt):
+    for j in range(i):
         # print("j :", j)
         print("*", end=" ")
     print()
+
+for i in range(1, n): 
+    # print("i :", i)
+    for j in range(n - i):
+        # print("j :", j)
+        print("*", end=" ")
+    print()
+
+
+# 방법 2 
+# cnt = 1
+# # print("cnt :", cnt)
+
+# for i in range(2*n-1): 
+#     # print("i :", i)
+#     for j in range(cnt):
+#         # print("j :", j)
+#         print("*", end=" ")
+#     print()
     
-    if i < n-1:
-        cnt += 1
-    else:
-        cnt -= 1
+#     if i < n-1:
+#         cnt += 1
+#     else:
+#         cnt -= 1
     
-    # print(cnt)
+#     # print(cnt)

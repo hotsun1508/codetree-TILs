d2280e88b0869a04367366b2ea846c96748efa51
Sun Ah Min <sunaslight1508@gmail.com>
2025-07-08 13:43:32 +0900

    Title: 별표 출력하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250708/별표 출력하기/print-star.py
+++ 250708/별표 출력하기/print-star.py
@@ -1,14 +1,14 @@
 n = int(input())
 
 # 방법 1 
-for i in range(1, n+1):
+for i in range(1, n+1):  # 1부터 하나씩 증가하는 직각삼각형 
     # print("i :", i)
     for j in range(i):
         # print("j :", j)
         print("*", end=" ")
     print()
 
-for i in range(1, n): 
+for i in range(1, n):  # n부터 하나씩 감소하는 직각삼각형 
     # print("i :", i)
     for j in range(n - i):
         # print("j :", j)
@@ -20,16 +20,16 @@ for i in range(1, n):
 # cnt = 1
 # # print("cnt :", cnt)
 
-# for i in range(2*n-1): 
+# for i in range(2*n-1):  # 직각삼각형 줄의 개수 (전체 세로 길이)
 #     # print("i :", i)
-#     for j in range(cnt):
+#     for j in range(cnt):  # 한 줄에 출력할 별의 개수 
 #         # print("j :", j)
 #         print("*", end=" ")
 #     print()
     
-#     if i < n-1:
+#     if i < n-1:  # 0부터 시작하니까 n-1보다 작을 때까지는 별의 개수 1씩 증가 
 #         cnt += 1
-#     else:
+#     else:        # n부터는 별의 개수 1씩 감소 
 #         cnt -= 1
     
 #     # print(cnt)

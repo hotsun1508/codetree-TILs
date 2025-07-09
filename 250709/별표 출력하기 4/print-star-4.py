b1d6f0cfd9497da43cd0c62a66cc30bab82c7b7f
Sun Ah Min <sunaslight1508@gmail.com>
2025-07-09 22:57:24 +0900

    Title: 별표 출력하기 4, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250709/별표 출력하기 4/print-star-4.py
+++ 250709/별표 출력하기 4/print-star-4.py
@@ -1,17 +1,36 @@
 n = int(input())
-cnt = 1
 
-for i in range(1, 2*n):
+# 방법 1 
+cnt = 1 
+
+for i in range(1, 2*n):  # 2n개 행만큼 출력 
     # print("i :", i)
-    for j in range(n-cnt+1):
+    for j in range(n-cnt+1):  # 별의 개수는 입력한 정수 n에서 cnt를 뺌으로써 거꾸로 range(a,b,-1)와 같은 결과 
         # print("j :", j)
         print("*", end=" ")
     print()
 
     # print("before cnt :", cnt)
-    if i < n:
+    if i < n:  # 행을 기준으로 cnt 증감 방향 바꾸기 
         cnt += 1
     else:
         cnt -= 1
     
     # print("after cnt :", cnt)
+
+
+# 방법 2
+# # 변수 선언 및 입력
+# n = int(input())
+
+# # 길이가 n인 직각삼각형을 뒤집어 출력합니다.
+# for i in range(n-1, -1, -1):
+# 	for _ in range(i+1):
+# 		print("*", end=" ")
+# 	print()
+	
+# # 길이가 n-1인 직각삼각형을 출력합니다.
+# for i in range(1, n):
+# 	for _ in range(i+1):
+# 		print("*", end=" ")
+# 	print()
